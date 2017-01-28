6bd1e64496733c6783df51d3e4e5a1bc7d2e775e
Geoffrey <geoffreyplichard@gmail.com>
2017-01-28 11:52:16 +0100

    create observable

--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
-alert('ok');
+import { Observable } from "rxjs";
+
+let circle = document.getElementById("circle");
+let source = Observable.fromEvent(document, "mousemove")
+    .map((e: MouseEvent) => {
+        return {
+            x: e.clientX,
+            y: e.clientY
+        }
+    })
+    .filter(value => value.x < 500)
+    .delay(300);
+
+function onNext(value) {
+    circle.style.left = value.x - 25 + "px";
+    circle.style.top = value.y - 25 + "px";
+}
+
+source.subscribe(
+    onNext,
+    e => console.log(`error: ${e}`),
+    () => console.log("complete")
+)
